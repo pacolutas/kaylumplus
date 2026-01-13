24960b319b0cf6d8a5ce093a55f68c1deceffc09
alter-ias <agencia.alter.ias@gmail.com>
2026-01-13 10:18:44 -0600

    fix(SearchInterface): remove orphaned code causing syntax error

--- src/lib/components/SearchInterface.svelte
+++ src/lib/components/SearchInterface.svelte
@@ -173,40 +173,7 @@
 				"Escucha tu música al instante o usa el botón de descarga para guardarla en tu dispositivo. ¡Disfruta de Kaylum Ultra!"
 		}
 	];
-			description:
-				"You can finally download and stream in Hi-Res again because of a much better API. It should also be much faster - try it out for yourself!"
-		},
-		{
-			title: 'Links support + QOLs!',
-			description:
-				"You can now paste links from supported streaming platforms (Spotify, YouTube, Apple Music, etc.) and the app will try to convert them to TIDAL equivalents for you to play or download. Only Spotify playlists work right now, but I'm working on fixing it."
-		},
-		{
-			title: 'Redesign + QQDL',
-			description:
-				'Hi-Res downloading still a WIP but a cool redesign that I inspired off a very cool library called Color Thief is here - and the site is also now up at QQDL!'
-		},
-		{
-			title: 'Hi-Res Audio',
-			description:
-				"Streaming for Hi-Res is now here. Stay tuned for Hi-Res downloading - I haven't gotten that one figured out yet. And video covers/lower quality streaming. Pretty cool."
-		},
-		{
-			title: 'Even more changes!',
-			description:
-				"LYRICS!!! I've stabilised the API a bit and added a few more features such as ZIP download of albums, better error handling, etc. Stay tuned for word by word lyrics!"
-		},
-		{
-			title: 'QOL changes',
-			description:
-				'This website is still very much in beta, but queue management and album/artist pages/downloads have been added as well as some bug squashing/QOL changes such as bigger album covers and download all for albums.'
-		},
-		{
-			title: 'Initial release!',
-			description:
-				"Two APIs fetch lossless CD-quality 16/44.1kHz FLACs. No support for Hi-Res yet but I'm working on it haha. No playlist saving or logging in either but downloading and streaming work."
-		}
-	];
+
 
 	const trackSkeletons = Array.from({ length: 6 }, (_, index) => index);
 	const gridSkeletons = Array.from({ length: 8 }, (_, index) => index);
